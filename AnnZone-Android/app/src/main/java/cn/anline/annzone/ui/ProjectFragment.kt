d2b5008fa6b31f1bb71ec80dc4d46092d9b151f7
Jiankian <luozhengan@vip.qq.com>
2018-02-05 08:40:29 +0800

    升级Kotlin到1.2.21，网络真差，搞了好几次不能升级插件今天终于可以升级了，跟1.2.10差不多

--- AnnZone-Android/app/src/main/java/cn/anline/annzone/ui/ProjectFragment.kt
+++ AnnZone-Android/app/src/main/java/cn/anline/annzone/ui/ProjectFragment.kt
@@ -30,7 +30,7 @@ class ProjectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
         rl_ProjectFragment.topPadding = ImmersionBar.getStatusBarHeight(activity)
 
         var projectDataItem:ArrayList<ProjectDataItem> = ArrayList<ProjectDataItem>()
